b0759e4b0c3fa33524d4db73931c02326538343c
andrea.cervesato <andrea.cervesato@u-blox.com>
2019-12-10 14:24:49 +0100

    added tokenizer for jobs

---
+++ kirk/tokenizer.py
@@ -0,0 +1,109 @@
+"""
+.. module:: tokenizer
+   :platform: Multiplatform
+   :synopsis: string -> job and vice-versa convertion
+.. moduleauthor:: Andrea Cervesato <andrea.cervesato@mailbox.org>
+"""
+import re
+
+
+class Tokenizer:
+    """
+    A generic string tokenizer.
+    """
+
+    def encode(self, project, job, params):
+        """
+        Encode project name, job name and job parameters into a unique
+        string identifier.
+        :param project: project name
+        :type project: str
+        :param job: job name
+        :type job: str
+        :param params: list of parameters with value
+        :type params: dict
+        :return: str
+        """
+        raise NotImplementedError()
+
+    def decode(self, token):
+        """
+        Decode a token string into project name, job name and job parameters.
+        :param token: token string
+        :type token: str
+        :return: (str, str, list(str))
+        """
+        raise NotImplementedError()
+
+
+class JobTokenizer(Tokenizer):
+    """
+    Tokenizer that encode job string as following:
+
+        project = "myproject"
+        job = "myjob"
+        params = dict(
+            param0="0",
+            param1="1",
+        )
+
+    becomes...
+
+        myproject::job[param0=0,param1=1]
+
+    """
+
+    def __init__(self):
+        self._re_pattern = re.compile(
+            r"(?P<project>\w+)::(?P<job>\w+)(?P<params>\[(\w+=\w+,?)*\])?"
+        )
+
+    def encode(self, project, job, params):
+        if not project:
+            raise ValueError("project name is empty")
+
+        if not job:
+            raise ValueError("job name is empty")
+
+        # convert parameters
+        params_arg = list()
+        for key, value in params.items():
+            arg = "%s=%s" % (key, value)
+            params_arg.append(arg)
+
+        # convert string
+        encoded = "%s::%s" % (project, job)
+        if params_arg:
+            arg = ",".join(params_arg)
+            encoded += "[%s]" % arg
+
+        return encoded
+
+    def decode(self, token):
+        if not token:
+            raise ValueError("token is empty")
+
+        # remove whitespaces
+        my_token = token.replace(" ", "")
+
+        # match for project::job[param0=0,param1=1]
+        match = self._re_pattern.match(my_token)
+        if not match:
+            return None
+
+        # get project and job name
+        project = match.group('project')
+        job = match.group('job')
+
+        # get parameters
+        params = dict()
+
+        match_params = match.group('params')
+        if match_params:
+            params_list = re.findall(r"(\w+=\w+)", match_params)
+
+            for param in params_list:
+                args = param.split("=")
+                params[args[0]] = args[1]
+
+        return project, job, params
---
+++ tests/test_tokenizer.py
@@ -0,0 +1,90 @@
+"""
+tokenizer module tests.
+"""
+import pytest
+from kirk.tokenizer import JobTokenizer
+
+
+class TestJobTokenizer:
+    """
+    Test JobTokenizer class
+    """
+
+    @pytest.fixture
+    def tokenizer(self):
+        """
+        Fixture exposing default tokenizer.
+        """
+        return JobTokenizer()
+
+    def test_encode_no_params(self, tokenizer):
+        """
+        Test encode method with a single parameter for one job
+        """
+        assert "myproject::myjob" == tokenizer.encode(
+            "myproject", "myjob", dict())
+
+    def test_encode_single_param(self, tokenizer):
+        """
+        Test encode method with a single parameter for one job
+        """
+        assert "myproject::myjob[param0=0]" == tokenizer.encode(
+            "myproject", "myjob", dict(param0="0"))
+
+    def test_encode_multiple_params(self, tokenizer):
+        """
+        Test encode method with multiple parameters for one job
+        """
+        assert "myproject::myjob[param0=0,param1=1]" == tokenizer.encode(
+            "myproject", "myjob", dict(param0="0", param1="1"))
+
+    def test_encode_errors(self, tokenizer):
+        """
+        Test encode method with bad formatted parameters for one job
+        """
+        with pytest.raises(ValueError, match="project name is empty"):
+            tokenizer.encode("", "myjob", dict())
+
+        with pytest.raises(ValueError, match="job name is empty"):
+            tokenizer.encode("myproject", "", dict())
+
+    def test_decode_no_params(self, tokenizer):
+        """
+        Test decode method with no params
+        """
+        assert ("myproject", "myjob", dict()) == tokenizer.decode(
+            "myproject::myjob")
+
+    def test_decode_single_param(self, tokenizer):
+        """
+        Test decode method with a single param
+        """
+        assert ("myproject", "myjob", dict(param0="0")) == tokenizer.decode(
+            "myproject::myjob[param0=0]")
+
+    def test_decode_multiple_params(self, tokenizer):
+        """
+        Test decode method with multiple params
+        """
+        assert ("myproject", "myjob", dict(param0="0", param1="1")) == tokenizer.decode(
+            "myproject::myjob[param0=0,param1=1]")
+
+    def test_decode_errors(self, tokenizer):
+        """
+        Test dencode method with bad formatted parameters for one job
+        """
+        with pytest.raises(ValueError, match="token is empty"):
+            tokenizer.decode("")
+
+    def test_decode_no_match(self, tokenizer):
+        """
+        Test dencode method when a bad token is given
+        """
+        assert tokenizer.decode("myproject:") == None
+
+    def test_decode_params_spaces(self, tokenizer):
+        """
+        Test dencode method when parameters are splitted by spaces
+        """
+        assert ("myproject", "myjob", dict(param0="0", param1="1")) == tokenizer.decode(
+            "myproject::myjob[param0=0,      param1=1]")
