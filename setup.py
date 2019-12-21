918c91be46d725893d32a643c5bc93d9df88f2e6
Andrea Cervesato <andrea.cervesato@mailbox.org>
2019-12-21 10:59:57 +0100

    moved main back to kirk folder

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'kirk=cmd.main:client',
+            'kirk=kirk.cmd:client',
         ],
     },
 )
