c02eaf43da9efc2d6a3aca1ca4670e2abd540aa3
Andrea Cervesato <andrea.cervesato@mailbox.org>
2019-12-22 16:42:19 +0100

    add non-python files to package

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'colorama',
     ],
     packages=find_packages(),
+    include_package_data=True,
     entry_points={
         'console_scripts': [
             'kirk=kirk.cmd:client',
