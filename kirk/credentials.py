9239c0aaf66d9d7cd544e16d949d1043d05bac31
Andrea Cervesato <andrea.cervesato@mailbox.org>
2019-12-29 11:01:11 +0100

    credentials documentation

--- kirk/credentials.py
+++ kirk/credentials.py
@@ -11,55 +11,55 @@ from kirk import KirkError
 
 class Credentials:
     """
-    Base class for a credentials handler
+    Base class for a credentials handler.
     """
 
     def get_password(self, section, username):
         """
         Return a password stored inside the keyrings file.
-        :param section: section of the username
-        :type section: str
-        :param username: user name
-        :type username: str
-        :return: password as string
+
+        Args:
+            section(str): section of the ``username``.
+            username(str): user name we want to get password.
+
+        Returns:
+            str: ``username`` password.
+
+        Raises:
+            :py:class:`KirkError`: raised if an error occurs.
         """
         raise NotImplementedError()
 
     def set_password(self, section, username, password):
         """
-        Set the user password inside keyrings file.
-        :param section: section of the username
-        :type section: str
-        :param username: user name
-        :type username: str
-        :param password: user password
-        :type username: str
+        Set the ``username`` password inside keyrings file.
+
+        Args:
+            section(str): section of the ``username``.
+            username(str): user name we want to set ``password``.
+            password(str): password of the ``username``.
+
+        Raises:
+            :py:class:`KirkError`: raised if an error occurs.
         """
         raise NotImplementedError()
 
 
 class CredentialsHandler(Credentials):
     """
-    Save/Load credentials from a file.
+    Inherit :py:class:`Credentials` and save/load credentials from a
+    keyrings file.
     """
 
     def __init__(self, file_path):
         """
-        :param file_path: keyrings file path
-        :type file_path: str
+        Args:
+            file_path(str): keyrings file path.
         """
         self._file_path = file_path
         self._inkr = PlaintextKeyring()
 
     def get_password(self, section, username):
-        """
-        Return a password stored inside the keyrings file.
-        :param section: section of the username
-        :type section: str
-        :param username: user name
-        :type username: str
-        :return: password as string
-        """
         password = ""
         try:
             self._inkr.file_path = self._file_path
@@ -70,15 +70,6 @@ class CredentialsHandler(Credentials):
         return password
 
     def set_password(self, section, username, password):
-        """
-        Set the user password inside keyrings file.
-        :param section: section of the username
-        :type section: str
-        :param username: user name
-        :type username: str
-        :param password: user password
-        :type username: str
-        """
         try:
             self._inkr.file_path = self._file_path
             self._inkr.set_password(section, username, password)
